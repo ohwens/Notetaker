da6738d97e899967c1e7657248704e2d7ecfaaf2
Ryan Owens <rtowens11@gmail.com>
2021-08-24 08:35:02 -0500

    htmlRoute change

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,7 +7,6 @@ module.exports = (app) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
   });
 
-  // If no matching route is found default to home
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
   });
