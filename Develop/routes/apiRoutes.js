a098f7641e63b69a2fe6dea21e1da0c02c406eed
Ryan Owens <rtowens11@gmail.com>
2021-08-23 22:22:04 -0500

    post route done plus new db.json with uuid

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -7,10 +7,17 @@ module.exports = (app) => {
 
 
     app.get("/api/notes", (req, res) => {
-        const notes = JSON.parse(fs.readFileSync('./db/db.json'));
-        res.json(notes)
+        const notesList = JSON.parse(fs.readFileSync('./db/db.json'));
+        res.json(notesList)
     });
 
+    app.post('/api/notes', (req, res) => {
+        const notesList = JSON.parse(fs.readFileSync('./db/db.json'));
+        const newNote = req.body;
+        newNote.id = uuid.v4();
+        notesList.push(newNote);
+        fs.writeFileSync('./db/db.json', JSON.stringify(notesList));
+        res.json(newNote);
 
+    });
 }
-
